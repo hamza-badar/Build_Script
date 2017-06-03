3ba793a2f17be173154a2f08170cc3373c8ce55f
hamza-badar <hamzabadar05@gmail.com>
2017-06-03 15:09:35 +0530

    add git clone

--- buildscript.sh
+++ buildscript.sh
@@ -3,16 +3,16 @@ echo "Welcome to Build Script"
 cd ~/rom
 
 echo "clonning trees"
-https://github.com/hamza-badar/device_motorola_otus -b 7.1.1 device/motorola/otus
+git clone https://github.com/hamza-badar/device_motorola_otus -b 7.1.1 device/motorola/otus
 
 echo "clonning common trees"
-https://github.com/hamza-badar/android_device_motorola_msm8610-common.git -b aex device/motorola/msm8610-common
+git clone https://github.com/hamza-badar/android_device_motorola_msm8610-common.git -b aex device/motorola/msm8610-common
 
 echo "clonning vendor"
 git clone https://github.com/TheMuppets/proprietary_vendor_motorola.git -b cm-14.1 vendor/motorola
 
 echo "clonning binaries"
-git https://github.com/hamza-badar/proprietary_vendor_qcom_binaries -b cm-14.1 vendor/qcom/binaries
+git clone git https://github.com/hamza-badar/proprietary_vendor_qcom_binaries -b cm-14.1 vendor/qcom/binaries
 
 echo "clonning kernel"
 git clone https://github.com/LineageOS/android_kernel_motorola_msm8610.git -b cm-14.1 kernel/motorola/msm8610
