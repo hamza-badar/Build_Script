3ba12ac1642a10445312f5a481f9a2f6bfa25698
hamza-badar <hamzabadar05@gmail.com>
2017-06-03 14:49:01 +0530

    update binaries

--- buildscript.sh
+++ buildscript.sh
@@ -12,7 +12,7 @@ echo "clonning vendor"
 git clone https://github.com/TheMuppets/proprietary_vendor_motorola.git -b cm-14.1 vendor/motorola
 
 echo "clonning binaries"
-git clone https://github.com/TheMuppets/proprietary_vendor_qcom_binaries.git -b cm-14.1 vendor/qcom/binaries
+git https://github.com/hamza-badar/proprietary_vendor_qcom_binaries -b cm-14.1 vendor/qcom/binaries
 
 echo "clonning kernel"
 git clone https://github.com/LineageOS/android_kernel_motorola_msm8610.git -b cm-14.1 kernel/motorola/msm8610
@@ -22,11 +22,6 @@ git clone https://github.com/LineageOS/android_packages_resources_devicesettings
 
 echo "clonning qcom/common"
 git clone https://github.com/LineageOS/android_device_qcom_common.git -b cm-14.1 device/qcom/common
-
-echo "removing binaries conflicts"
-cd vendor/qcom/binaries
-git revert 63fbdf157d98c52a7ad33fde539c841feb566b92
-cd ~/rom
 
 echo "changing host and user"
 export KBUILD_BUILD_USER=Hamza_Badar
